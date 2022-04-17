0f7b261ba3529e44467b9275ee453f230b87a3a3
Roger Zhang <1787009521@qq.com>
2022-04-17 17:41:16 +0800

    feat: Observable对象

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import { fromEvent } from 'rxjs';
+import { fromEvent } from "rxjs";
+import { filter } from 'rxjs/operators';
 
 const $input = document.querySelector('.todo-val')
 
-const input$ = fromEvent($input, 'keydown')
+const input$ = fromEvent($input, 'keydown') // Observable对象
 
-input$.subscribe(x => console.log(x));
+// filter: 筛选出keyCode !== 13 的keyDown数组
+const inputKeyCode13$ = input$.pipe(filter((r) =>  r.keyCode === 13 )) // Observable对象
+
+
+inputKeyCode13$.subscribe(x => console.log(x));
